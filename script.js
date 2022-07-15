fad1e8adb020dcf972106438d5f88e28cd43b6cc
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-07-15 19:51:30 +0100

    completed the rock paper scissors game with an extra choice of how many game do you want to play to win, next I need to tidy up the code split everything up into separate functions.

--- script.js
+++ script.js
@@ -1,55 +1,115 @@
-function computerPlay()
+
+let playerwins = 0, computerwins = 0;
+let reqWins = parseInt(prompt("Till how many wins are we playing?")); 
+
+function numToRps (func)
 {
-    let x = Math.floor((Math.random() * 3) + 1);
-    return x;
+    switch (func)
+    {
+        case 1:
+            return "Rock!";
+        case 2:
+            return "Paper!";
+        case 3: 
+            return "Scissors!";
+    }
 }
 
-function playerPlay()
-{
-    let x = parseInt(prompt("choose your option: 1 rock, 2 paper, 3 scissors"));
-    return x;
-}
+const button = document.querySelectorAll("button");
+button.forEach(but => but.addEventListener("click", playRound));
 
-function playRound()
+function whoWins(computer, player)
 {
-    let computer = computerPlay();
-    let player = playerPlay();
-    console.log(computer);
-    console.log(player);
     switch (true)
     {
+
         case computer == 1 && player == 1:
-            return "even"; 
+
+            return "Even"; 
         case computer == 2 && player == 2:
-            return "even"; 
+
+            return "Even"; 
         case computer == 3 && player == 3:
-            return "even"; 
+
+            return "Even"; 
         case computer == 1 && player == 2:
+            playerwins++;
+ 
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Player wins"; 
         case computer == 2 && player == 3:
+            playerwins++;
+
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Player wins"; 
         case computer == 3 && player == 1:
+            playerwins++;
+
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Player wins";
         case computer == 2 && player == 1:
+            computerwins++;
+
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Computer Wins"; 
         case computer == 3 && player == 2:
+            computerwins++;
+
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Computer Wins"; 
         case computer == 1 && player == 3:
+            computerwins++;
+
+            score.textContent = `${playerwins} : ${computerwins}`;
             return "Computer Wins";
         default: break;
     }
 }
-
-function game()
-{   let Count = 0; 
-    for(let i = 0; i < 5; i++)
-    {
-        if(playRound() == "Computer Wins")
-        {
-            Count++;
-        }  
-    }
-    if(Count >=3){return "Computer wins";}
-    if(Count < 3){return "Player wins";}
-    if(Count == 0){return "Draw";}
+function disabled(){
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach(but => but.disabled = true);
 }
+function enabled(){
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach(but => but.disabled = false);
+}
+
+function restart(){
+    console.log("restart")
+    restrt = document.querySelector(".restart");
+    rbutton = document.createElement("button");
+    rbutton.textContent = "Restart Game?";
+    restrt.appendChild(rbutton);
+    rbutton.addEventListener("click", () => {
+        const score = document.querySelector("#score"); score.textContent = "0 : 0";
+        playerwins = 0;
+        computerwins = 0;
+        let ptext = document.querySelector("#player");
+        ptext.textContent = "Your Choice";
+        let ctext = document.querySelector("#computer");
+        ctext.textContent = "Computers Choice";
+        rbutton.remove();
+        enabled();
+    })
+
+}
+
+function playRound(e)
+{
+    let computer = Math.floor((Math.random() * 3) + 1);
+    let player = e.target.id;
+
+    let ptext = document.querySelector("#player");
+        ptext.textContent = e.target.textContent;
+
+    let ctext = document.querySelector("#computer");
+        ctext.textContent = numToRps(computer);
+    console.log(computer);
+    console.log(player);
+
+console.log(whoWins(computer, player));
+
+    if(playerwins == reqWins){const score = document.querySelector("#score"); score.textContent = "PLAYER WINS!"; disabled(); restart()}
+    if(computerwins == reqWins){const score = document.querySelector("#score"); score.textContent = "COMPUTER WINS!"; disabled(); restart()}
+}
+
