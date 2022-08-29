0833933a3fee3333d49f589ac2067b2a9d930916
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-08-29 18:29:16 +0100

    added the win or draw screens added the control buttons for rock paper scissors next step is to add the restart or exit game functionality

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const startGame = document.querySelector(".start");
 const numOfGames = document.querySelector(".games");
 const enter = document.querySelector(".enter");
 const inactive = document.querySelectorAll(".inactive");
+const rock = document.querySelector(".rock");
+const scissors = document.querySelector(".scissors");
+const paper = document.querySelector(".paper");
+const gameControls = document.querySelector(".gamePopup");
+const playerWinScreen = document.querySelector(".playerWins");
+const computerWinScreen = document.querySelector(".computerWins");
+const drawScreen = document.querySelector(".draw");
+const restartGame = document.querySelectorAll(".restartGame");
+const exit = document.querySelectorAll(".exit");
+let computerWinsCount = 0;
+let playerWinsCount = 0; 
+let gamesCount = 0;
+rock.addEventListener("click", (e) => {
+    playRound(e);
+    game();
+})
+scissors.addEventListener("click", (e) => {
+    playRound(e);
+    game();
+})
+paper.addEventListener("click", (e) => {
+    playRound(e);
+    game();
+})
 startGame.addEventListener("click", openPopup);
 enter.addEventListener("click", initializeGame);
 
@@ -19,54 +43,75 @@ function computerPlay()
     return x;
 }
 
-function playerPlay()
-{
-    let x = parseInt(prompt("choose your option: 1 rock, 2 paper, 3 scissors"));
-    return x;
-}
+// function playerPlay()
+// {
+//     let x = parseInt(prompt("choose your option: 1 rock, 2 paper, 3 scissors"));
+//     return x;
+// }
 
-function playRound()
+function playRound(event)
 {
+    gamesCount++;
+    let player = event.target.classList;
     let computer = computerPlay();
-    let player = playerPlay();
+    // let player = playerPlay();
     console.log(computer);
-    console.log(player);
+    // console.log(player);
     switch (true)
     {
-        case computer == 1 && player == 1:
+        case computer == 1 && player == "rock":
+            console.log("even");
             return "even"; 
-        case computer == 2 && player == 2:
+        case computer == 2 && player == "paper":
+            console.log("even");
             return "even"; 
-        case computer == 3 && player == 3:
+        case computer == 3 && player == "scissors":
+            console.log("even");
             return "even"; 
-        case computer == 1 && player == 2:
+        case computer == 1 && player == "paper":
+            playerWinsCount++;
+            console.log("player");
             return "Player wins"; 
-        case computer == 2 && player == 3:
+        case computer == 2 && player == "scissors":
+            console.log("player");
+            playerWinsCount++;
             return "Player wins"; 
-        case computer == 3 && player == 1:
+        case computer == 3 && player == "rock":
+            playerWinsCount++;
+            console.log("player");
             return "Player wins";
-        case computer == 2 && player == 1:
+        case computer == 2 && player == "rock":
+            computerWinsCount++;
+            console.log("computer");
             return "Computer Wins"; 
-        case computer == 3 && player == 2:
+        case computer == 3 && player == "paper":
+            computerWinsCount++;
+            console.log("computer");
             return "Computer Wins"; 
-        case computer == 1 && player == 3:
+        case computer == 1 && player == "scissors":
+            computerWinsCount++;
+            console.log("computer");
             return "Computer Wins";
         default: break;
     }
 }
 
 function game()
-{   let Count = 0; 
-    for(let i = 0; i < numOfGames.value; i++)
-    {
-        if(playRound() == "Computer Wins")
-        {
-            Count++;
-        }  
+{  
+    // for(let i = 0; i < numOfGames.value; i++)
+    // {
+    //     if(playRound() == "Computer Wins")
+    //     {
+    //         Count++;
+    //     }  
+    // }
+    if(computerWinsCount > numOfGames.value/2) computerWins();
+    if(playerWinsCount > numOfGames.value/2) playerWins();
+    if(gamesCount == numOfGames.value){
+        if(playerWinsCount > computerWinsCount) playerWins();
+        if(playerWinsCount < computerWinsCount) computerWins();
+        if(playerWinsCount == computerWinsCount) draw();
     }
-    if(Count >= numOfGames.value/2){console.log("comp"); return "Computer wins";}
-    if(Count < numOfGames.value/2){console.log("pl"); return "Player wins";}
-    if(Count == 0){console.log("dr"); return "Draw";}
 }
 
 function initializeGame(){
@@ -74,5 +119,18 @@ function initializeGame(){
         elem.classList.remove("active");
     })
     startGame.classList.add("active");
-    game();
+    gameControls.classList.add("active");
+}
+
+function playerWins(){
+    playerWinScreen.classList.add("active");
+    gameControls.classList.remove("active");
+}
+function computerWins(){
+   computerWinScreen.classList.add("active");
+   gameControls.classList.remove("active");
+}
+function draw(){
+    drawScreen.classList.add("active");
+    gameControls.classList.remove("active");
 }
